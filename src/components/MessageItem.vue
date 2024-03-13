e642b2b311d8cdf3f2a01073f97df4604e523210
nambs <angelonambs@gmail.com>
2024-03-13 20:19:45 +0300

    message vue

--- src/components/MessageItem.vue
+++ src/components/MessageItem.vue
@@ -1,37 +1,41 @@
 <template>
-  <div class="columns mx-0 p-0">
-    <div class="column is-4 p-0 avatar">
-      <svg height="75" width="75" class="p-0">
-        <circle cx="37.5" cy="37.5" r="30" stroke="white" stroke-width="3" />
-      </svg>
-    </div>
-    <div class="column p-0 messageText">
-      <p>{{ nameUser() }}</p>
-      <p>{{ props.message.text }}</p>
-    </div>
+  <div class="content">
+    <p>{{ temps }}</p>
+    <p :class="destinataire() ? 'textMessage destinataire' : 'textMessage'">{{ props.message.text }}</p>
   </div>
 </template>
 
 <script setup>
-import { useAuthStore } from '@/stores/auth.store'
+import { useAuthStore } from '@/stores/auth.store';
 
+const auth = useAuthStore().me
 const props = defineProps(['message'])
-const me = useAuthStore().me
 
-const nameUser = () => {
-  return me._id === props.message.from._id ? props.message.to.name : props.message.from.name
+const temps = (new Date(props.message.createdAt)).toLocaleString()
+
+const destinataire = () => {
+  if(auth._id == props.message.to)
+    return true
+  else return false
 }
 </script>
 
 <style scoped>
-.avatar {
-  display: grid;
-  align-content: center;
-  justify-content: center;
+.content {
+  text-align: center;
+  width: 100%;
 }
-.messageText {
-  display: grid;
-  align-content: center;
-  justify-content: start;
+.textMessage {
+  background: black;
+  color: rgba(255, 255, 255, 0.868);
+  border-radius: 20px;
+  padding: 5px 10px;
+  height: max-content;
+  width: max-content;
+}
+
+.destinataire {
+  background: #00C4A7;
+  float: right;
 }
 </style>
