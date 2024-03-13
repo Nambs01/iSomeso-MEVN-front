aa439797ac4191f7625fb6d487775b618660ad63
nambs <angelonambs@gmail.com>
2024-03-13 20:26:23 +0300

    implementation discussion & message

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,17 @@
 <template>
   <main>
-    <Navbar :user="auth.me" />
+    <Navbar />
 
     <div class="columns hero is-fullheight-with-navbar is-multiline mx-0">
       <div class="column hero is-3-desktop is-4-tablet is-12-mobile is-black">
         <div class="row p-4">
           <SearchBar />
-          <ListMessages :messages="messagesStore.messages" />
+          <ListDiscussions @userChated="ouvrirDiscussion" />
         </div>
       </div>
 
-      <div class="column main">
-        <div class="title is-3 has-text-primary">
+      <div :class="'column' + (!userChated.values ? ' main' : '')">
+        <div v-if="!userChated.values" class="title is-3 has-text-primary">
           <p>
             <span>Nouveau message</span>
             <span class="icon mx-4">
@@ -19,24 +19,35 @@
             </span>
           </p>
         </div>
+
+        <ListeMessages v-else :user="userChated.values" />
+        <!-- </RouterView> -->
       </div>
     </div>
   </main>
 </template>
 
 <script setup>
+import { reactive } from 'vue'
+import { useUserStore } from '@/stores/user.store'
+import { useMessageStore } from '@/stores/message.store'
 import Navbar from '../components/Navbar.vue'
 import SearchBar from '../components/SearchBar.vue'
-import ListMessages from '../components/ListMessages.vue'
-import { useAuthStore } from '@/stores/auth.store'
-import { useMessageStore } from '@/stores/message.store'
+import ListDiscussions from '../components/ListeDiscussions.vue'
+import ListeMessages from '@/components/ListeMessages.vue'
 import { onMounted } from 'vue'
 
-const auth = useAuthStore()
-const messagesStore = useMessageStore()
+const userChated = reactive({})
+
+const ouvrirDiscussion = (data) => {
+  if (userChated.values != data) {
+    userChated.values = data
+    useMessageStore().getMessages(data._id)
+  }
+}
 
 onMounted(() => {
-  messagesStore.getLastMessages()
+  useUserStore().getAllUsers()
 })
 </script>
 
